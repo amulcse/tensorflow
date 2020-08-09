880fabc5591405f214d56aed587aac1265f765ee
Amul Patel <amul@jetbro.in>
2020-08-09 16:13:44 +0530

    model with 3 dense layer

--- celsious_to_fahrenheit.py
+++ celsious_to_fahrenheit.py
@@ -6,3 +6,22 @@ logger.setLevel(logging.ERROR)
 
 celsius_q    = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
 fahrenheit_a = np.array([-40,  14, 32, 46, 59, 71.6, 100.4],  dtype=float)
+
+l0 = tf.keras.layers.Dense(units=4, input_shape=[1])
+l1 = tf.keras.layers.Dense(units=4)
+l2 = tf.keras.layers.Dense(units=1)
+
+model = tf.keras.Sequential([l0, l1, l2])
+
+model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+
+model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+
+print("Finished training the model")
+
+print(model.predict([100.0]))
+
+print("Model predicts that 100 degrees Celsius is: {} degrees Fahrenheit".format(model.predict([100.0])))
+print("These are the l0 variables: {}".format(l0.get_weights()))
+print("These are the l1 variables: {}".format(l1.get_weights()))
+print("These are the l2 variables: {}".format(l2.get_weights()))
