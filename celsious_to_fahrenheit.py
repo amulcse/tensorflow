aca1326c8dacc368a8dd2b20e19faa799e1954d4
Amul Patel <amul@jetbro.in>
2020-08-09 15:46:44 +0530

    Initialise with basic model

---
+++ celsious_to_fahrenheit.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+import numpy as np
+import logging
+logger = tf.get_logger()
+logger.setLevel(logging.ERROR)
+
+celsius_q    = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
+fahrenheit_a = np.array([-40,  14, 32, 46, 59, 71.6, 100.4],  dtype=float)
